d689029dfa72fa13992389330bcda2b41d84c3ec
dgajaria11 <dakshgajaria@gmail.com>
2025-04-03 11:18:47 -0400

    new daksh commits 3

--- src/intro.tsx
+++ src/intro.tsx
@@ -9,7 +9,8 @@ export function Intro({name1, name2} :Names) {
             <h2>About Us</h2>
             <p>
                 {name1} is a sophomore CS and BCB major at WPI, and he is 20 years old. He is from southern New Hampshire,
-                where he has lived for his whole life up until college. {name2}
+                where he has lived for his whole life up until college. {name2} is a sophomore CS major at WPI as well, he is
+                from Eastern Mass.
             </p>
         </div>
     )
